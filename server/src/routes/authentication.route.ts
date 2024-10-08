b9df2b405c0fe2e8837d39a573758b54028db84e
NguyenDangTrieu <nguyendangtrieu110803@gmail.com>
2024-10-08 23:01:05 +0700

    reup

--- server/src/routes/authentication.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { login, register } from '../controllers/authentication.controller';
-
-export default (router: express.Router) => {
-    router.post('/auth/register', register);
-
-    router.patch('/auth/login', login);
-}
