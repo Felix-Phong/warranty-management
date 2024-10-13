ec0ee0b02ff976760aea51f8f3ed7ddb024d7d5c
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-13 15:58:01 +0700

    Confirm warranty registration

--- server/src/controllers/warrantyHistory.controller.ts
+++ server/src/controllers/warrantyHistory.controller.ts
@@ -1,6 +1,53 @@
 import express from 'express';
 
 import warrantyHistoryModel from '../Models/warrantyHistory.model';
+import WarrantyRegistrationModel from '../Models/warrantyRegistration.model';
+import { UserModel } from '../Models/user.model';
+
+// Định nghĩa kiểu cho status để sử dụng lại
+type Status = "pending_check" | "under_repair" | "returned_to_customer" | "sent_to_manufacturer";
+
+export const confirmWarrantyRegistration = async (req: express.Request, res: express.Response) {
+    const techId = req.params.techId;
+    const warrantyRegisID = req.params.warrantyRegisID;
+    const newStatus = req.query.newStatus;
+
+    const validStatuses = ["under_repair", "returned_to_customer", "sent_to_manufacturer"];
+
+    try {
+        if (!techId || !newStatus || !warrantyRegisID) {
+            return res.status(400).json({ message: 'Missing required parameters!' });
+        }
+
+        if (!validStatuses.includes(newStatus as Status)) {
+            return res.status(400).json({ message: 'Invalid status value!' });
+        }
+
+        const tech = await UserModel.findOne({ _id: techId, role: "staff technical" });
+        if (!tech) {
+            return res.status(400).json({ message: "Technician not found!" });
+        }
+
+        const warrantyRegis = await WarrantyRegistrationModel.findOne({ _id: warrantyRegisID });
+        if (!warrantyRegis) {
+            return res.status(400).json({ message: "Warranty registration not found!" });
+        }
+
+        warrantyRegis.current_status = newStatus as Status;
+        await warrantyRegis.save();
+
+        if (!req.body) {
+            return res.status(400).json({ message: "Can't save empty content to Warranty history!" });
+        }
+
+        const warrantyHistory = new warrantyHistoryModel(req.body);
+        const savedWarrantyHistory = await warrantyHistory.save();
+
+        return res.status(201).json(savedWarrantyHistory);
+    } catch (error) {
+        return res.status(500).json({ message: 'Error fetching products', error });
+    }
+}
 
 export const createWarrantyHistory = async (req: express.Request, res: express.Response) => {
     try {
