f9d4e1452ddb1d288b6970c7e44d632b2082bbf3
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-13 18:04:03 +0700

    Get products by category

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 
-import { createProduct, deleteProductById, getAllProducts, getProductById, updateProductById } from '../controllers/product.controller';
+import { 
+    createProduct, deleteProductById, getAllProducts,
+    getProductById, updateProductById , getProductsByCategory
+} from '../controllers/product.controller';
 
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
+    router.get('/products/getProductsByCategory/:category', isAuthenticated, getProductsByCategory)
+
     router.post('/products', isAuthenticated, createProduct);
 
     router.get('/products', isAuthenticated, getAllProducts);
